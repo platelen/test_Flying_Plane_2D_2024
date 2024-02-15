76f487cde30ad0d5e859ea39ebd119129c96a262
Dmitry <platelen@gmail.com>
2024-02-15 16:15:45 +0300

    Health_1
    Настроил спавн аптечек в заданной области и с интервалом.

---
+++ Assets/Scripts/Health_Kit/SoHealthKitData.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Health_Kit
+{
+    [CreateAssetMenu(menuName = "Create Health Kit Data", fileName = "Health Kit Data")]
+    public class SoHealthKitData : ScriptableObject
+    {
+        [SerializeField] private float _minSpawnIntervalBig = 30f;
+        [SerializeField] private float _maxSpawnIntervalBig = 60f;
+        [SerializeField] private float _minSpawnIntervalSmall = 7f;
+        [SerializeField] private float _maxSpawnIntervalSmall = 15f;
+
+        public float MinSpawnIntervalBig => _minSpawnIntervalBig;
+
+        public float MaxSpawnIntervalBig => _maxSpawnIntervalBig;
+
+        public float MinSpawnIntervalSmall => _minSpawnIntervalSmall;
+
+        public float MaxSpawnIntervalSmall => _maxSpawnIntervalSmall;
+    }
+}
